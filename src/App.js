acab0982c4bf301c225a645548b9117e64caf601
kanoi-y <furafuratonbo@gmail.com>
2021-08-10 21:29:31 +0900

    apiからデータを取得して表示

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import Article from './components/Article';
-import logo from './logo.svg';
 
 function App() {
+  const [id, setId] = useState('deatiger');
+  const [name, setName] = useState('');
+  const ids = ['deatiger', 'gaearon', 'aws', 'google', 'facebook'];
+
+  const getRandomId = () => {
+    const _id = ids[Math.floor(Math.random() * ids.length)];
+    setId(_id);
+  };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/users/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setName(data.name);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [id]);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <p>
+        {id}の、GitHub上の名前は{name} です。
+      </p>
+      <button onClick={getRandomId}>IDを変更</button>
+      {/* <div>{btnCount}</div>
+      <button onClick={CountUp}>count up!</button>
+      <button onClick={CountDown}>count down!</button> */}
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
@@ -24,7 +51,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
